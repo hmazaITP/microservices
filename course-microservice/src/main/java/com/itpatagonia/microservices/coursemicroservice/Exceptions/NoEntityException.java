63bfc21ce8a4cc47ffd994d918a416029edcf08b
hmazaITP <hmaza@itpatagonia.com>
2022-06-14 00:25:32 -0300

    Primer Commit

--- course-microservice/src/main/java/com/itpatagonia/microservices/coursemicroservice/Exceptions/NoEntityException.java
+++
@@ -1,7 +0,0 @@
-package com.itpatagonia.microservices.coursemicroservice.Exceptions;
-
-public class NoEntityException extends Exception{
-    public NoEntityException(String message){
-        super(message);
-    }
-}
